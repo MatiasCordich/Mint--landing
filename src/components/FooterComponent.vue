08d4d1b95cffd9a0495b57d4e606067a3bee7216
MatiasCordich <m.scordich@gmail.com>
2024-03-24 20:25:56 -0300

    Footer finished

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,18 +1,156 @@
 <template>
     <footer class="footer">
-        Footer
+        <div class="logo">
+            <img width="40" height="40" src="https://img.icons8.com/fluency-systems-regular/48/0f0f0f/mint.png"
+                alt="mint" />
+            <h2>Mint!</h2>
+        </div>
+        <div class="footer_menues">
+            <ul class="menu_footer">
+                <h2>Resources</h2>
+                <li>
+                    <RouterLink to="/">Home</RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/pricing">Pricing</RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/services">Services</RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/solutions">Solutions</RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/questions">FAQ</RouterLink>
+                </li>
+            </ul>
+            <ul class="footer_links">
+                <li>
+                    <img src="https://img.icons8.com/ios-filled/40/0f0f0f/twitterx--v1.png" alt="twitterx--v1" />
+                </li>
+                <li>
+                    <img src="https://img.icons8.com/ios-filled/40/0f0f0f/instagram-new--v1.png"
+                        alt="instagram-new--v1" />
+                </li>
+                <li>
+                    <img src="https://img.icons8.com/ios-filled/40/0f0f0f/linkedin.png" alt="linkedin" />
+                </li>
+
+            </ul>
+            <ul class="footer_links-aparts">
+                <li>
+                    Mint! Inc. All rights Reserved
+                </li>
+                <li>
+                    Terms and Conditions
+                </li>
+                <li>
+                    Privacy Policy
+                </li>
+            </ul>
+        </div>
+
     </footer>
 </template>
 
 <script>
-
+import { RouterLink } from 'vue-router';
 
 </script>
 
 <style>
-.footer{
+.footer {
     width: 100%;
-    height: 10rem;
-    margin-top: 10rem;
+    height: 100%;
+    margin-top: 5rem;
+    padding: 3rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+}
+
+.logo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: .5rem;
+    width: 100%;
+}
+
+.footer_menues{
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    width: 100%;
+}
+
+.menu_footer {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 4rem;
+    font-weight: 600;
+}
+
+.menu_footer h2 {
+    font-weight: 900;
+}
+
+.footer_links {
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
+}
+
+.footer_links-aparts {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+    font-size: 1.5rem;
+    font-weight: 600;
+}
+
+.footer_links-aparts li {
+    text-decoration: underline var(--font-color);
+}
+
+@media (min-width: 900px) {
+
+    .footer{
+        width: 80%;
+        margin: 0 auto;
+        margin-top: 8rem;
+    }
+    .logo{
+        justify-content: flex-start;
+    }
+
+    .footer_menues{
+        flex-direction: row;
+        justify-content: space-around;
+        gap: 9rem;
+    }
+
+    .menu_footer{
+        width: fit-content;
+        gap: 2rem;
+    }
+
+    .menu_footer > * {
+        width: 100%;
+        text-align: left;
+    }
+
+    .footer_links-aparts li{
+        width: 100%;
+        text-align: left;
+    }
+
+    
+
+
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -29,8 +29,8 @@
         <form>
           <input type="email" placeholder="Email">
           <button class="btn_form" type="submit">
-            <img class="btn_form-icon" width="50" height="50" src="https://img.icons8.com/ios-filled/50/circled-left-2.png"
-              alt="circled-left-2" />
+            <img class="btn_form-icon" width="50" height="50"
+              src="https://img.icons8.com/ios-filled/50/circled-left-2.png" alt="circled-left-2" />
           </button>
         </form>
       </div>
@@ -57,7 +57,56 @@
         Infinite possibilities
       </h2>
       <section class="benefits_container">
-        <section class="benefits_description"></section>
+        <section class="benefits_description">
+          <h2>Be part of us.</h2>
+          <div class="line-2"></div>
+          <p>
+            To be part of Mint! opens up a world full of possibilities, complete financial freedom, and most
+            importantly, being your own bank. Because our customers are our trust quota.
+          </p>
+          <h2>What benefits will you obtain?</h2>
+          <div class="line-2"></div>
+          <p>
+            Being part of Mint! will provide you with benefits in the following areas:
+          </p>
+          <ul class="benefits-list">
+            <li>
+              <img src="https://img.icons8.com/ios-glyphs/15/0f0f0f/checkmark--v1.png" alt="checkmark--v1" />
+              Customer
+              service
+            </li>
+            <li>
+              <img src="https://img.icons8.com/ios-glyphs/15/0f0f0f/checkmark--v1.png" alt="checkmark--v1" />
+              Security
+            </li>
+            <li>
+              <img src="https://img.icons8.com/ios-glyphs/15/0f0f0f/checkmark--v1.png" alt="checkmark--v1" />
+              Financial Services
+            </li>
+            <li>
+              <img src="https://img.icons8.com/ios-glyphs/15/0f0f0f/checkmark--v1.png" alt="checkmark--v1" />
+              Cryptocurrencies
+            </li>
+          </ul>
+
+          <h2>
+            Your account, your world.
+          </h2>
+          <div class="line-2"></div>
+          <p>
+            Opening a bank account at Mint! is entirely personal, so your account is your world, and we will provide you with personalized customer service.
+          </p>
+          <h2>Start with $0</h2>
+          <div class="line-2"></div>
+          <p class="benefit_text">
+            When you create an account with Mint!, the cost will be $0 dollars. You can continue with the same or access our plans.
+            <span>
+              <RouterLink to="/prices">
+                Our plans
+              </RouterLink>
+            </span>
+          </p>
+        </section>
         <section class="benefits_grid">
           <CardIcon v-for="(card, index) in cardsIcon" :key="index" :imageUrl="card.imageUrl" :title="card.title"
             :text="card.text" :cardClass="card.cardClass" :altDescription="card.altDescription" />
@@ -316,13 +365,60 @@ export default {
 
 .benefits_container {
   display: flex;
-  flex-wrap: wrap;
+  flex-direction: column;
+  gap: 2rem;
   width: 100%;
 }
 
 .benefits_description {
   width: 100%;
-  height: 90rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+
+.benefits_description h2 {
+  font-size: 4rem;
+  font-weight: 900;
+}
+
+.line-2 {
+  width: 95%;
+  height: .1rem;
+  background-color: var(--font-color);
+}
+
+.benefits_description p {
+  line-height: 4rem;
+}
+
+.benefits-list {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.benefits-list li {
+  display: flex;
+  align-items: center;
+  gap: .5rem;
+}
+
+.benefit_text{
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+}
+
+.benefit_text span{
+  width: fit-content;
+  background-color: var(--font-color);
+  border-radius: 3rem;
+}
+
+.benefit_text a{
+  padding: 1rem 2rem;
+  color: var(--font-negative-color);
 }
 
 
@@ -398,6 +494,8 @@ export default {
   .benefits_container {
     flex-direction: row;
   }
+
+
 
   .benefits_grid,
   .benefits_description {
